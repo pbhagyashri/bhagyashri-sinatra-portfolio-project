dc1d0b91e3129ec344edd0fe5af235006637299c
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-08-14 20:29:51 +0000

    Add login form to users/login.erb. can login a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,29 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
+    @user = User.create(params[:user])
+    session[:id] = @user.id
+    redirect '/companies/companies'
+  end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:user][:username], email: params[:user][:email])
+    if @user
+      session[:id] = @user.id
+      session[:username] = @user.username
+      session[:email] = @user.email
+      session[:password] = @user.password
+      redirect to '/companies'
+    end
+
+  end
+
+  get '/companies' do
+    "All the companies"
   end
 
 end
